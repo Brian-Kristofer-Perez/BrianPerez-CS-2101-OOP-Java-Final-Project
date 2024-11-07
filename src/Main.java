fd36b74ee28cd79d67651138cfcf304c4688d8d7
Brian <23-01884@g.batstate-u.edu.ph>
2024-11-07 22:01:20 +0800

    Finally finished main menu UI i think???

--- src/Main.java
+++ src/Main.java
@@ -17,9 +17,15 @@ public class Main {
             System.out.println("1. Log in");
             System.out.println("2. Sign up");
             System.out.println("3. Exit");
-
             System.out.print("Input your choice: ");
-            choice = input.nextLine().charAt(0);
+
+            // catch empty input errors!
+            try {
+                choice = input.nextLine().charAt(0);
+            } catch (Exception e) {
+                System.out.println("Please provide a proper input.");
+                continue;
+            }
 
             switch(choice) {
                 case '1':
@@ -45,7 +51,15 @@ public class Main {
             System.out.println("2. Employer");
 
             System.out.print("Input your choice (0 to return): ");
-            choice = input.nextLine().charAt(0);
+
+
+            // catch empty input errors!
+            try {
+                choice = input.nextLine().charAt(0);
+            } catch (Exception e) {
+                System.out.println("Please provide a valid input.");
+                continue;
+            }
 
             switch (choice) {
                 case '0':
@@ -55,7 +69,7 @@ public class Main {
                     workerRegister(input);
                     break;
                 case '2':
-                    // employerRegister()
+                    employerRegister(input);
                     break;
                 default:
                     System.out.println("Please provide a valid input.");
@@ -73,7 +87,14 @@ public class Main {
             System.out.println("2. Employer");
 
             System.out.print("Input your choice (0 to return): ");
-            choice = input.nextLine().charAt(0);
+
+            // catch empty input errors!
+            try {
+                choice = input.nextLine().charAt(0);
+            } catch (Exception e) {
+                System.out.println("Please provide a valid input.");
+                continue;
+            }
 
             switch (choice) {
                 case '0':
@@ -83,7 +104,7 @@ public class Main {
 
                     return;
                 case '2':
-                    // employerLogin()
+                    employerLogin(input);
                     return;
                 default:
                     System.out.println("Please provide a valid input.");
@@ -94,14 +115,14 @@ public class Main {
     private static void workerLogin(Scanner input){
 
         // initialize variables
-        String email, password;
+        String username, password;
 
         // collect user input
         while (true) {
-            System.out.print("Input your email (leave blank to return): ");
-            email = input.nextLine();
+            System.out.print("Input your username (leave blank to return): ");
+            username = input.nextLine();
 
-            if(email.isEmpty()){
+            if(username.isEmpty()){
                 return;
             }
 
@@ -117,8 +138,8 @@ public class Main {
                 Connection db = DriverManager.getConnection("jdbc:mysql://localhost:3306/testschema", "root", "12345678");
 
                 // check if the user exists
-                PreparedStatement query = db.prepareStatement("SELECT * FROM WORKERS WHERE email = ? AND password = ?;");
-                query.setString(1, email);
+                PreparedStatement query = db.prepareStatement("SELECT * FROM WORKERS WHERE username = ? AND password = ?;");
+                query.setString(1, username);
                 query.setString(2, password);
                 ResultSet resultSet = query.executeQuery();
 
@@ -132,7 +153,7 @@ public class Main {
 
                 // else break
                 else {
-                    System.out.println("No records found. Email or password may be incorrect");
+                    System.out.println("No records found. username or password may be incorrect");
                 }
 
             } catch (SQLException e) {
@@ -142,27 +163,17 @@ public class Main {
 
     }
 
-    private static void employerLogin(){
-        /*Input email
-         * input password
-         * if email and password IN database:
-         *   login, construct object
-         * else:
-         *   error!
-         * */
-    }
+    private static void employerLogin(Scanner input){
 
-
-    private static void workerRegister(Scanner input){
         // initialize variables
-        String email, password;
+        String username, password;
 
         // collect user input
         while (true) {
-            System.out.print("Input your email (leave blank to return): ");
-            email = input.nextLine();
+            System.out.print("Input your username (leave blank to return): ");
+            username = input.nextLine();
 
-            if(email.isEmpty()){
+            if(username.isEmpty()){
                 return;
             }
 
@@ -177,17 +188,67 @@ public class Main {
             try {
                 Connection db = DriverManager.getConnection("jdbc:mysql://localhost:3306/testschema", "root", "12345678");
 
-                // check if the person's email exists in db
-                PreparedStatement query = db.prepareStatement("SELECT * FROM WORKERS WHERE email = ?;");
-                query.setString(1, email);
+                // check if the user exists
+                PreparedStatement query = db.prepareStatement("SELECT * FROM employers WHERE username = ? AND password = ?;");
+                query.setString(1, username);
+                query.setString(2, password);
+                ResultSet resultSet = query.executeQuery();
+
+                // if user is found!
+                if (resultSet.next()) {
+                    System.out.println("Logged in!");
+
+                    // add constructors here!
+                    break;
+                }
+
+                // else break
+                else {
+                    System.out.println("No records found. username or password may be incorrect");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static void workerRegister(Scanner input){
+        // initialize variables
+        String username, password;
+
+        // collect user input
+        while (true) {
+            System.out.print("Input your username (leave blank to return): ");
+            username = input.nextLine();
+
+            if(username.isEmpty()){
+                return;
+            }
+
+            System.out.print("Input your password (leave blank to return): ");
+            password = input.nextLine();
+
+            if(password.isEmpty()){
+                return;
+            }
+
+            // connect to database
+            try {
+                Connection db = DriverManager.getConnection("jdbc:mysql://localhost:3306/testschema", "root", "12345678");
+
+                // check if the person's username exists in db
+                PreparedStatement query = db.prepareStatement("SELECT * FROM WORKERS WHERE username = ?;");
+                query.setString(1, username);
                 ResultSet resultSet = query.executeQuery();
 
                 // if user is valid (doesn't exist yet)!
                 if (!resultSet.next()) {
 
                     // add to DB
-                    PreparedStatement statement = db.prepareStatement("INSERT INTO workers (email, password) VALUES (?, ?);");
-                    statement.setString(1, email);
+                    PreparedStatement statement = db.prepareStatement("INSERT INTO workers (username, password) VALUES (?, ?);");
+                    statement.setString(1, username);
                     statement.setString(2, password);
                     statement.executeUpdate();
                     System.out.println("Added successfully!");
@@ -196,7 +257,59 @@ public class Main {
 
                 // else, error
                 else {
-                    System.out.println("Email already exists. Please log in.");
+                    System.out.println("Username already exists.");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static  void employerRegister(Scanner input){
+        // initialize variables
+        String username, password;
+
+        // collect user input
+        while (true) {
+            System.out.print("Input your username (leave blank to return): ");
+            username = input.nextLine();
+
+            if(username.isEmpty()){
+                return;
+            }
+
+            System.out.print("Input your password (leave blank to return): ");
+            password = input.nextLine();
+
+            if(password.isEmpty()){
+                return;
+            }
+
+            // connect to database
+            try {
+                Connection db = DriverManager.getConnection("jdbc:mysql://localhost:3306/testschema", "root", "12345678");
+
+                // check if the person's username exists in db
+                PreparedStatement query = db.prepareStatement("SELECT * FROM employers WHERE username = ?;");
+                query.setString(1, username);
+                ResultSet resultSet = query.executeQuery();
+
+                // if user is valid (doesn't exist yet)!
+                if (!resultSet.next()) {
+
+                    // add to DB
+                    PreparedStatement statement = db.prepareStatement("INSERT INTO employers (username, password) VALUES (?, ?);");
+                    statement.setString(1, username);
+                    statement.setString(2, password);
+                    statement.executeUpdate();
+                    System.out.println("Added successfully!");
+                    break;
+                }
+
+                // else, error
+                else {
+                    System.out.println("Username already exists.");
                 }
 
             } catch (SQLException e) {
